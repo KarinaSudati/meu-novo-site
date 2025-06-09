35f0fb66164f2e1a5570e147fd40a0f05c16ed84
karinasudati <karinaadssudati@gmail.com>
2025-06-09 18:09:25 -0300

    add comment in function js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-document.getElementById("botaoEnviar").addEventListener("click", function(event) {
-  event.preventDefault(); 
-  validarFormulario();
- });
-
-  function validarFormulario() {
-     if(document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
-     alert("Prontinho! Você receberá as novidades por email.");
-
-    }else{
-        alert("Por favor, preencha os campos nome e email!");
+document
+  .getElementById("botaoEnviar")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    validarFormulario();
+  });
+// Função para validar formulário de envio
+function validarFormulario() {
+  if (
+    document.getElementById("nome").value != "" &&
+    document.getElementById("email").value != ""
+  ) {
+    alert("Prontinho! Você receberá as novidades por email.");
+  } else {
+    alert("Por favor, preencha os campos nome e email!");
   }
 }
-
-  
-
-
-
-
-
- 
