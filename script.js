5286ce40f1ddde7aa9a5627ae48cb5f96accec99
karinasudati <karinaadssudati@gmail.com>
2025-06-09 20:57:17 -0300

    fix format input

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ document
   });
 // Função para validar formulário de envio
 function validarFormulario() {
-  if (
-    document.getElementById("nome").value != "" &&
-    document.getElementById("email").value != ""
-  ) {
-    alert("Prontinho! Você receberá as novidades por email.");
+  
+    const nome = document.getElementById("nome").value.trim(); 
+    const email = document.getElementById("email").value.trim();
+    if (nome !== "" && email !== "") {
+      alert("Prontinho! Você receberá as novidades por email.");
   } else {
     alert("Por favor, preencha os campos nome e email!");
   }
-}
+    }
+   
+    
+  
+
